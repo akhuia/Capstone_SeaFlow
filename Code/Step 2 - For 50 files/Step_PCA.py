64ee4a30ec3b15476c17bb15a7e805f99892185b
eltond <eltond@uw.edu>
2015-04-30 00:28:38 -0700

    PCA + Clustering
    Added Clustering with PCA

--- Code/Step 2 - For 50 files/Step_PCA.py
+++ Code/Step 2 - For 50 files/Step_PCA.py
@@ -3,6 +3,7 @@ import os.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pprint
 
 def CreateList(Locale):
     ListOfFiles = []
@@ -10,7 +11,7 @@ def CreateList(Locale):
         ListOfFiles.append(name)
     return ListOfFiles
 
-def Run_PCA(f):
+def Data_Clus(f):
     FigFile = f[:-3]+"png"
     if FigFile.find("Tok") == -1:
         col = "blue"
@@ -27,16 +28,45 @@ def Run_PCA(f):
     transformed = newsource.as_matrix().dot(matrix_w)
     plt.plot(transformed[0:len(source),0],transformed[0:len(source),1],\
          'o', markersize=7, color=col, alpha=0.5, label='class1')  
+    #for i in range(2,7):
+         #   kmeans_model = KMeans(n_clusters=i, random_state=1).fit(transformed)
+          #  labels = kmeans_model.labels_  
+           # print i, metrics.silhouette_score(transformed, labels, metric='euclidean')
+    kmeans = cluster.KMeans(n_clusters=2)
+    kmeans.fit(transformed)    
+    labels = kmeans.labels_
+    centroids = kmeans.cluster_centers_
+    for i in range(2):
+        ds = transformed[np.where(labels==i)]
+        plt.plot(ds[:,0],ds[:,1],'o')
+        lines = plt.plot(centroids[i,0],centroids[i,1],'kx')
+        plt.setp(lines,ms=15.0)
+        plt.setp(lines,mew=2.0)
     plt.savefig(FigFile)
-    plt.clf()
-
+    plt.clf()      
+    return ((eig_pairs[0]+eig_pairs[1]), f[f.find("armb"):])   
 
 def main():
     Images = []
+    MatrixDict = {}
+    MatrixDict['File'] = []
+    MatrixDict['Eigenvalue1'] = []
+    MatrixDict['Eigenvector1'] = [] 
+    MatrixDict['Eigenvalue2'] = []
+    MatrixDict['Eigenvector2'] = []    
     path = "C:\Users\NYU\SeaFlow2\*"
     FileList = CreateList(path)
     for i in FileList:
-        Images = Run_PCA(i)       
+        Images.append(Data_Clus(i))
+    for i,j in enumerate(Images):
+        MatrixDict['File'].append(Images[i][1])
+        MatrixDict['Eigenvalue1'].append(Images[i][0][0])
+        MatrixDict['Eigenvector1'].append(Images[i][0][1]) 
+        MatrixDict['Eigenvalue2'].append(Images[i][0][2])
+        MatrixDict['Eigenvector2'].append(Images[i][0][3])
+    FinalMatrix = pd.DataFrame(MatrixDict, columns=['File','Eigenvalue1','Eigenvalue2','Eigenvector1','Eigenvector2'])
+    FinalMatrix.to_csv("Capstone.csv")
+    print FinalMatrix
 
 
 if __name__ == "__main__":
